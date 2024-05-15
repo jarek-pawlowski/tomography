0a22b24c38a47672729166a660775bc104afb08a
jarek <jarek@ruton>
2024-05-15 22:36:22 +0200

    fixed tensordot used as kron

--- derandomized_tomo.py
+++ derandomized_tomo.py
@@ -8,7 +8,7 @@ ms = utils.Measurement(utils.Kwiat, 2)
 
 directory = './derandomized_test/single'
 os.makedirs(directory, exist_ok=True)
-test_states = [utils.tensordot(ms.basis[i], ms.basis[j], indices=0).reshape((4,4)) for j in [0,1,2,3] for i in [0,1,2,3]]
+test_states = [utils.tensordot(ms.basis[i], ms.basis[j], indices=0, moveaxis=(1,2)).reshape((4,4)) for i in [0,1,2,3] for j in [0,1,2,3]]
 for i, state in enumerate(test_states):
     np.save(os.path.join(directory, 'm_'+str(i)), state)
 
@@ -18,7 +18,7 @@ for i in [0,1,2,3]:
     for j in [0,1,2,3]:
         for i1 in [0,1,2,3]:
             for j1 in [0,1,2,3]:
-                state = utils.tensordot(ms.basis[i], ms.basis[j], indices=0) + utils.tensordot(ms.basis[i1], ms.basis[j1], indices=0)
+                state = utils.tensordot(ms.basis[i], ms.basis[j], indices=0, moveaxis=(1,2)) + utils.tensordot(ms.basis[i1], ms.basis[j1], indices=0, moveaxis=(1,2))
                 np.save(os.path.join(directory, 'm_'+str(i*4+j)+'_'+str(i1*4+j1)), state.reshape((4,4))/2.)
 
 directory = './derandomized_test/Xs'
